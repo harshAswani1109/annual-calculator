986cd88a00e762d6a9c06abcc91525a0689aaae1
harshAswani1109 <ha1109@srmist.edu.in>
2024-12-20 14:30:52 +0530

    buy added

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -7,13 +7,13 @@ const Navbar: React.FC = () => {
   const pathname = usePathname(); // Get the current path
 
   return (
-    <nav className={`fixed top-0 left-0 w-full flex justify-between items-center p-4 text-white z-10 ${pathname === "/buy" ? "bg-blue-500" : "bg-green-500"}`}>
+    <nav className={`fixed top-0 left-0 w-full flex justify-between items-center p-4 text-white z-10 bg-white/30 backdrop-blur-md rounded-b-lg shadow-lg ${pathname === "/buy" ? "" : ""}`}>
       {/* Mobile View */}
       <div className="flex md:hidden justify-around text-2xl items-center w-full"
       >
         <Link
-          href="/buy"
-          className={`mr-4 ${pathname === "/buy" ? "underline" : ""}`}
+          href="/"
+          className={`mr-4 ${pathname === "/" ? "underline" : ""}`}
         >
           Buy
         </Link>
@@ -30,8 +30,8 @@ const Navbar: React.FC = () => {
         <div className="text-lg">Hello World</div>
         <div>
           <Link
-            href="/buy"
-            className={`mr-4 ${pathname === "/buy" ? "underline" : ""}`}
+            href="/"
+            className={`mr-4 ${pathname === "/" ? "underline" : ""}`}
           >
             Buy
           </Link>
